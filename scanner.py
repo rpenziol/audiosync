a67b6c023a0f9d7ef2c86dde48f260459e2d3f29
Robbie Penziol <rpenziol@users.noreply.github.com>
2017-01-19 22:53:33 -0800

    Added function to remove orphaned files from output folder

--- scanner.py
+++ scanner.py
@@ -43,6 +43,7 @@ Depending on file type, copy or convert file from input_path to output_path
 def dir_scanner(input_path='', output_path='', options=None):
         # Remove orphaned folder trees from output_path
         remove_orphan_dirs(input_path, output_path)
+        remove_orphan_files(input_path, output_path)
 
         # Convert files in current directory
         for item in os.listdir(input_path):
@@ -72,10 +73,38 @@ def remove_orphan_dirs(input_path='', output_path=''):
         if not os.path.isfile(os.path.join(output_path, item)):
             # If directory is orphaned
             if not os.path.exists(os.path.join(input_path, item)):
-
                 log.debug("'{0}' is an orphaned directory. Removing from '{1}'.".format(item, output_path))
                 try:
                     shutil.rmtree(os.path.join(output_path, item))
                 except Exception as e:
                     print(e)
                     log.warning("Failed to remove folder '{0}'.".format(os.path.join(output_path, item)))
+
+
+''' Remove files from output_path if file isn't in input_path'''
+
+
+def remove_orphan_files(input_path='', output_path=''):
+    # Store list of file names (without extensions) from input_path
+    input_files = []
+
+    # Collect list of file names from input_path
+    for item in os.listdir(input_path):
+        if os.path.isfile(os.path.join(input_path, item)):
+            # Strip file extension
+            base_name = os.path.splitext(item)[0]
+            input_files.append(base_name)
+
+    # Remove orphaned files from output_path
+    for item in os.listdir(output_path):
+        if os.path.isfile(os.path.join(output_path, item)):
+            # Strip file extension
+            base_name = os.path.splitext(item)[0]
+            # If file is orphaned
+            if base_name not in input_files:
+                log.debug("'{0}' is an orphaned file. Removing from '{1}'.".format(item, output_path))
+                try:
+                    os.remove(os.path.join(output_path, item))
+                except Exception as e:
+                    print(e)
+                    log.warning("Failed to remove file '{0}'.".format(os.path.join(output_path, item)))
