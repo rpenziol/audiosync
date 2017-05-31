05df5b5ae29103b082962b570b16059353b3b06b
Robbie Penziol <rpenziol@users.noreply.github.com>
2017-05-30 21:40:25 -0700

    Multi-processing file convertions! Got around Python limitation of not being able to call class functions with a pool

--- converter.py
+++ converter.py
@@ -6,6 +6,60 @@ import shlex
 from multiprocessing import Pool
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
+temp_db = None
+
+''' Use FFmpeg wrapper to convert file from fqfn_input to fqfn_output destination.
+Grab formatting options from options JSON '''
+def convert(job):
+    fqfn_input = job['input_file']
+    fqfn_output = job['output_file']
+    fqfn_input_base, input_extension = os.path.splitext(fqfn_input)
+    fqfn_input_md5 = hashlib.md5(open(fqfn_input, 'rb').read()).hexdigest()
+    hash_match = False
+
+    # Check if file hash is in the database
+    try:
+        if temp_db.get_hash(fqfn_input) == fqfn_input_md5:
+            hash_match = True
+    except Exception as e:
+        log.debug(e)
+        log.debug("File '%s' hash not in database" % fqfn_input)
+
+    # Check if output file exists and is up to date
+    if hash_match and os.path.isfile(fqfn_output):
+        log.debug("File '%s' already exists. Skipping." % fqfn_output)
+        return
+
+    # Check if output file exists and remove it, as it's outdated
+    if os.path.isfile(fqfn_output):
+        log.info("File '%s' is outdated. Deleting." % fqfn_output)
+        os.remove(fqfn_output)
+
+    # Store/update hash
+    temp_db.update(fqfn_input, fqfn_input_md5)
+
+    # Simply copy non-audio files
+    if input_extension != '.flac':
+        try:
+            log.info("Copying '%s' to '%s'" % (fqfn_input, fqfn_output))
+            shutil.copy2(fqfn_input, fqfn_output)
+        except Exception as e:
+            print(e)
+            log.warning("Insufficient privileges to write file: '%s'." % fqfn_output)
+        return
+
+    # Do the conversion
+    try:
+        log.info("Converting file '%s'." % fqfn_input)
+        command = 'ffmpeg -i ' + shlex.quote(fqfn_input) + ' -ab 320k -map_metadata 0 -id3v2_version 3 ' + \
+                  shlex.quote(fqfn_output) + '>/dev/null 2>&1'
+        os.system(command)
+
+    except Exception as e:
+        print(e)
+        log.warning("Insufficient privileges to write file: '%s'." % fqfn_output)
+
+    return
 
 
 class Converter(object):
@@ -27,57 +81,9 @@ class Converter(object):
         self.queue.append(job)
 
     def process_queue(self):
-        pool = Pool(processes=1)
-        for job in self.queue:
-            self.convert(job['input_file'], job['output_file'])
-
-    ''' Use FFmpeg wrapper to convert file from fqfn_input to fqfn_output destination.
-    Grab formatting options from options JSON '''
-    def convert(self, fqfn_input='', fqfn_output=''):
-        fqfn_input_base, input_extension = os.path.splitext(fqfn_input)
-        fqfn_input_md5 = hashlib.md5(open(fqfn_input, 'rb').read()).hexdigest()
-        hash_match = False
-
-        # Check if file hash is in the database
-        try:
-            if self.db.get_hash(fqfn_input) == fqfn_input_md5:
-                hash_match = True
-        except Exception as e:
-            log.debug(e)
-            log.debug("File '%s' hash not in database" % fqfn_input)
-
-        # Check if output file exists and is up to date
-        if hash_match and os.path.isfile(fqfn_output):
-            log.debug("File '%s' already exists. Skipping." % fqfn_output)
-            return
-
-        # Check if output file exists and remove it, as it's outdated
-        if os.path.isfile(fqfn_output):
-            log.info("File '%s' is outdated. Deleting." % fqfn_output)
-            os.remove(fqfn_output)
-
-        # Store/update hash
-        self.db.update(fqfn_input, fqfn_input_md5)
-
-        # Simply copy non-audio files
-        if input_extension != '.flac':
-            try:
-                log.info("Copying '%s' to '%s'" % (fqfn_input, fqfn_output))
-                shutil.copy2(fqfn_input, fqfn_output)
-            except Exception as e:
-                print(e)
-                log.warning("Insufficient privileges to write file: '%s'." % fqfn_output)
-            return
-
-        # Do the conversion
-        try:
-            log.info("Converting file '%s'." % fqfn_input)
-            command = 'ffmpeg -i ' + shlex.quote(fqfn_input) + ' -ab 320k -map_metadata 0 -id3v2_version 3 ' + \
-                      shlex.quote(fqfn_output) + '>/dev/null 2>&1'
-            os.system(command)
-
-        except Exception as e:
-            print(e)
-            log.warning("Insufficient privileges to write file: '%s'." % fqfn_output)
-
-        return
+        global temp_db  # Hack to pass the pool multiple arguments, given that convert() can only have 1 arg
+        temp_db = self.db
+        pool = Pool(processes=8)
+        pool.map(convert, self.queue)
+        pool.close()
+        pool.join()
